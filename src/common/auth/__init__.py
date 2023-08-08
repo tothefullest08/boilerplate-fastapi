994830f148a2db418bc6d3ff2ecef8000a1b2af0
Harry <tothefullest08@gmail.com>
2023-08-09 01:33:49 +0900

    [Feat] JWT 토큰 validation 로직 구현

--- src/common/auth/__init__.py
+++ src/common/auth/__init__.py
@@ -1,12 +1,20 @@
 from datetime import datetime, timedelta
 
+from sqlalchemy.orm import Session
+from typing_extensions import Annotated
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+from jose import jwt, JWTError
 
 from src.common.config import config
+from src.common.database import get_db
+from src.common.exception import InternalException, FailureType
+from src.common.logger import Logger
+from src.user.repository.user_repository import UserRepository
 
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="api/v1/users/sign-in", scheme_name="JWT", auto_error=False
+)
 
 
 class JwtTokenizer:
@@ -25,3 +33,40 @@ class JwtTokenizer:
             algorithm=config.JWT_ALGORITHM,
         )
         return token
+
+    @staticmethod
+    def decode(
+        token: str,
+        key: str = config.ACCESS_TOKEN_SECRET,
+    ) -> dict:
+        return jwt.decode(
+            token,
+            key=key,
+            algorithms=config.JWT_ALGORITHM,
+        )
+
+
+async def get_current_user_id(
+    token: Annotated[str, Depends(oauth2_scheme)], session: Session = Depends(get_db)
+) -> int:
+    try:
+        payload = JwtTokenizer.decode(token, key=config.ACCESS_TOKEN_SECRET)
+        user_id: int = payload.get("user_id")
+        if user_id is None:
+            Logger().error(f" 헤더 토큰에 유저 아이디 없음. token: {token}")
+            raise InternalException(FailureType.UNAUTHORIZED_TOKEN_ERROR, "")
+    except JWTError:
+        Logger().error(f" 토큰 시그니처가 유효하지 않음. token: {token}")
+        raise InternalException(FailureType.UNAUTHORIZED_TOKEN_ERROR)
+    except Exception:
+        Logger().error(f" 그외 토큰 에러. token: {token}")
+        raise InternalException(FailureType.UNAUTHORIZED_TOKEN_ERROR)
+
+    user_token = UserRepository(session_=session).get_user_token(user_id)
+    if user_token.access_token != token:
+        Logger().error(
+            f" 헤더의 토큰 값이 DB의 토큰값과 일치하지 않음. token: {token}, user_id: {user_id}"
+        )
+        raise InternalException(FailureType.UNAUTHORIZED_TOKEN_ERROR)
+
+    return user_id
--- src/common/exception/failure_type.py
+++ src/common/exception/failure_type.py
@@ -6,10 +6,11 @@ class FailureType(Enum):
     INVALID_REQUEST_ERROR = ("invalid_request_error", 400)
     CREATE_DATA_ERROR = ("create_data_error", 400)
     GET_DATA_ERROR = ("get_data_error", 400)
-    UPSERT_DATA_ERROR = ("usert_data_error", 400)
+    UPSERT_DATA_ERROR = ("upsert_data_error", 400)
     UPDATE_DATA_ERROR = ("update_data_error", 400)
     DELETE_DATA_ERROR = ("delete_data_error", 400)
     NOT_AUTHORIZED_ERROR = ("not_authorized_error", 401)
+    UNAUTHORIZED_TOKEN_ERROR = ("unauthorized_token_error", 401)
 
     @property
     def error_type(self) -> str:
