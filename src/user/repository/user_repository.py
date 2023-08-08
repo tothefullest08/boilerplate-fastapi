2324ed8a8c1132593e4b3a918acda2fd0b485537
Harry <tothefullest08@gmail.com>
2023-08-09 00:37:35 +0900

    [Feat] user 비즈니스 로직 구현
    로그인, 로그아웃, 회원가입 로직 구현

--- src/user/repository/user_repository.py
+++ src/user/repository/user_repository.py
@@ -1,18 +1,116 @@
+from typing import List, Union
 from sqlalchemy.orm import Session
-
 from src.common.exception import InternalException, FailureType
 from src.common.logger import Logger
-from src.user.model.user_model import UserModel
+from src.user.model.user_model import UserModel, UserTokenModel
 
 
 class UserRepository:
-    def __init__(self, session: Session, logger: Logger = Logger()):
-        self.__session = session
+    def __init__(self, session_: Session, logger: Logger = Logger()):
+        self.__session = session_
         self.__logger = logger
 
-    def get_users(self) -> [UserModel]:
+    def create_user(self, phone_number: str, password: str) -> UserTokenModel:
         try:
-            return self.__session.query(UserModel).all()
+            user: UserModel = UserModel(
+                phone_number=phone_number,
+                password=password,
+            )
+            self.__session.add(user)
+            self.__session.commit()
+
+            return user
+
         except Exception as e:
-            self.__logger.error(f"유저 목록 조회 실패, e: {e}")
+            self.__session.rollback()
+            self.__session.flush()
+
+            self.__logger.error(f" 유저 생성 실패. phone_number: {phone_number}, e: {e}")
+            raise InternalException(FailureType.CREATE_DATA_ERROR, "유저 생성 실패")
+
+    def get_users(
+        self, user_ids: List[int] = None, phone_number: str = None
+    ) -> [UserModel]:
+        try:
+            filter_ = []
+            if user_ids:
+                filter_.append(UserModel.user_id.in_(user_ids))
+            if phone_number:
+                filter_.append(UserModel.phone_number == phone_number)
+
+            return self.__session.query(UserModel).filter(*filter_).all()
+        except Exception as e:
+            self.__logger.error(f"유저 목록 조회 실패, user_ids: {user_ids}, e: {e}")
             raise InternalException(FailureType.GET_DATA_ERROR, "유저 목록 조회 실패")
+
+    def get_user(
+        self, user_id: int = None, phone_number: str = None
+    ) -> Union[UserModel, None]:
+        user_ids = [user_id] if user_id else None
+        users = self.get_users(user_ids=user_ids, phone_number=phone_number)
+        if not users:
+            return None
+        return users[0]
+
+    def upsert_user_token(
+        self, user_id: int, access_token: int, refresh_token: int
+    ) -> UserTokenModel:
+        try:
+            user_token = (
+                self.__session.query(UserTokenModel)
+                .filter(UserTokenModel.user_id == user_id)
+                .first()
+            )
+            if user_token:
+                user_token.access_token = access_token
+                user_token.refresh_token = refresh_token
+
+            else:
+                user_token: UserTokenModel = UserTokenModel(
+                    user_id=user_id,
+                    access_token=access_token,
+                    refresh_token=refresh_token,
+                )
+            self.__session.add(user_token)
+            self.__session.commit()
+
+            return user_token
+
+        except Exception as e:
+            self.__session.rollback()
+            self.__session.flush()
+
+            self.__logger.error(f" 유저 토큰 upsert 실패. user_id: {user_id}, e: {e}")
+            raise InternalException(FailureType.UPSERT_DATA_ERROR, "유저 토큰 upsert 실패")
+
+    def get_user_token(self, user_id: int = None) -> Union[UserTokenModel, None]:
+        try:
+            return (
+                self.__session.query(UserTokenModel)
+                .filter(UserTokenModel.user_id == user_id)
+                .first()
+            )
+        except Exception as e:
+            self.__logger.error(f"유저 토큰 조회 실패, user_id: {user_id}, e: {e}")
+            raise InternalException(FailureType.GET_DATA_ERROR, "유저 토큰 조회 실패")
+
+    def delete_user_token(self, user_id: int) -> None:
+        try:
+            removed_count = (
+                self.__session.query(UserTokenModel)
+                .filter(UserTokenModel.user_id == user_id)
+                .delete(synchronize_session="fetch")
+            )
+            if not removed_count:
+                self.__session.flush()
+                return 0
+
+            self.__session.commit()
+
+            return removed_count
+
+        except Exception as e:
+            self.__session.rollback()
+            self.__session.flush()
+            self.__logger.error(f" 유저 토큰 삭제 실패. user_id: {user_id}, e: {e}")
+            raise InternalException(FailureType.UPDATE_DATA_ERROR, "유저 토큰 삭제 실패")
--- src/user/service/user_service.py
+++ src/user/service/user_service.py
@@ -1,24 +1,114 @@
-from typing import List
-
+import re
 from sqlalchemy.orm import Session
-
-from src.user.model.user_model import UserModel
+from src.common.auth import JwtTokenizer
+from src.common.cipher import Cipher
+from src.common.config import config
+from src.common.exception import FailureType, InternalException
+from src.common.logger import Logger
+from src.user.interface.user_dto import JwtTokenDto, UserTokenDto
+from src.user.model.user_model import UserTokenModel, UserModel
 from src.user.repository.user_repository import UserRepository
 
 
 class UserService:
-    def __init__(self, session: Session):
-        self.__session = session
-        self.__user_repo = UserRepository(session=session)
+    def __init__(self, session_: Session, logger: Logger = Logger()):
+        self.__user_repo = UserRepository(session_=session_)
+        self.__logger = logger
 
-    def get_users(self) -> List[UserModel]:
-        return self.__user_repo.get_users()
+    def sign_in(self, phone_number: str, password: str) -> UserTokenDto:
+        user = self.__validate_existing_user(phone_number)
+        self.__verify_password(plain_password=password, hashed_password=user.password)
 
-    def sign_in(self):
-        pass
+        jwt_token = self.__create_jwt_token(user_id=user.id)
+        user_token = self.__user_repo.upsert_user_token(
+            user_id=user.id,
+            access_token=jwt_token.access_token,
+            refresh_token=jwt_token.refresh_token,
+        )
 
-    def sign_up(self):
-        pass
+        return UserTokenDto(
+            id=user_token.id,
+            user_id=user_token.user_id,
+            access_token=user_token.access_token,
+            refresh_token=user_token.refresh_token,
+            created_at=user_token.created_at,
+            updated_at=user_token.updated_at,
+        )
 
-    def sign_out(self):
-        pass
+    def __validate_existing_user(self, phone_number: str) -> UserModel:
+        user = self.__user_repo.get_user(phone_number=phone_number)
+        if not user:
+            raise InternalException(FailureType.NOT_AUTHORIZED_ERROR, "회원 정보가 없음")
+
+        return user
+
+    def __verify_password(self, plain_password: str, hashed_password: str) -> None:
+        res = Cipher.verify(plain_password, hashed_password)
+        if not res:
+            raise InternalException(FailureType.NOT_AUTHORIZED_ERROR, "비밀번호 불일치")
+
+    def sign_up(self, phone_number: str, password: str) -> UserTokenDto:
+        self.__validate_phone_number(phone_number)
+        self.__validate_new_user(phone_number)
+        encrypted_password = self.__encrypt_password(password)
+        user = self.__user_repo.create_user(
+            phone_number=phone_number, password=encrypted_password
+        )
+
+        jwt_token = self.__create_jwt_token(user_id=user.id)
+        user_token = self.__user_repo.upsert_user_token(
+            user_id=user.id,
+            access_token=jwt_token.access_token,
+            refresh_token=jwt_token.refresh_token,
+        )
+
+        return UserTokenDto(
+            id=user_token.id,
+            user_id=user_token.user_id,
+            access_token=user_token.access_token,
+            refresh_token=user_token.refresh_token,
+            created_at=user_token.created_at,
+            updated_at=user_token.updated_at,
+        )
+
+    def __validate_phone_number(self, phone_number: str) -> None:
+        pattern = re.compile("^010-[0-9]{4}-[0-9]{4}$")
+        if not pattern.match(phone_number):
+            raise InternalException(
+                FailureType.INPUT_PARAMETER_ERROR, "전화번호 양식이 잘못되었습니다."
+            )
+
+    def __validate_new_user(self, phone_number: str) -> None:
+        user = self.__user_repo.get_user(phone_number=phone_number)
+        if user:
+            raise InternalException(FailureType.NOT_AUTHORIZED_ERROR, "이미 유저 정보가 존재함")
+
+    def __encrypt_password(self, password: str) -> str:
+        return Cipher.encrypt(password)
+
+    def sign_out(self, user_id: int) -> None:
+        self.__user_repo.delete_user_token(user_id)
+
+    def __create_jwt_token(
+        self,
+        user_id: int,
+    ) -> JwtTokenDto:
+        access_token = JwtTokenizer.encode(
+            payload={
+                "user_id": user_id,
+                "sub": "access_token",
+            },
+            token_expire_minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES,
+            key=config.ACCESS_TOKEN_SECRET,
+        )
+
+        refresh_token = JwtTokenizer.encode(
+            payload={
+                "user_id": user_id,
+                "sub": "refresh_token",
+            },
+            token_expire_minutes=config.REFRESH_TOKEN_EXPIRE_MINUTES,
+            key=config.REFRESH_TOKEN_SECRET,
+        )
+
+        return JwtTokenDto(access_token=access_token, refresh_token=refresh_token)
